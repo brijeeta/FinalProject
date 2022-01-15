ff2b1c0b352604732f95ba06ee0d688fa10ce459
Holy Paul <brijeeta@gmail.com>
2022-01-15 16:02:21 -0500

    added server DB connection files,sample resolvers,typedefs and packages

---
+++ server/config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+// Enable access to .env variables
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmediaapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
